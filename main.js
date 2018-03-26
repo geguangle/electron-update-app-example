2952b81c1c40931ba439e78968c3bc121b79e4c1
Maurice Hofman <maurice@mahowebdesign.nl>
2018-03-26 11:07:20 +0200

    added env variable

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const {app, BrowserWindow, Menu, protocol, ipcMain} = require('electron');
 const log = require('electron-log');
 const {autoUpdater} = require("electron-updater");
 
-
+process.env.APPIMAGE = 'production';
 
 //-------------------------------------------------------------------
 // Logging
